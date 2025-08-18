4194cf8c90e02c692d14cd9ab35ba01808de02fd
Nguyen Van Hoai <nguyenvanhoai1020@gmail.com>
2025-08-18 21:58:06 +0700

    add customer table in customer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,23 @@
 // A lots of thing to do
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
-export default function Page() {
-   return <p>Customer Page</p>;
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+   title: 'Customers',
+};
+
+export default async function Page(props: {
+   searchParams?: Promise<{
+      query?: string;
+      page?: string;
+   }>;
+}) {
+   const searchParams = await props.searchParams;
+   const query = searchParams?.query || '';
+
+   const customers = await fetchFilteredCustomers(query);
+
+   return <CustomersTable customers={customers} />;
 }
